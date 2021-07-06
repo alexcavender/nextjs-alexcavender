a1a6e6fc2b3ce3db7a8f8f2825eb54f9352cf1a3
Alex Cavender <alexcavender@gmail.com>
2021-07-06 18:28:59 -0400

    btn effects

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <div className={utilStyles.headerGrid}>
           <div>
             <h2 className={utilStyles.heading2XL}>Hi, I'm Alex.</h2>
-            <p className={utilStyles.headingLg}>I'm a Web Developer based in Vermont.</p>
+            <h3 className={utilStyles.headingLg}>I'm a Web Developer based in Vermont.</h3>
             <p>I specialize in JavaScript (React & Vue), WordPress .</p>
             <a className={utilStyles.primaryBtn}
             href="mailto:alexcavender@gmail.com?Subject=Hello"
