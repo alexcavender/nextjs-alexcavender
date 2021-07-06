598f6681c2e22ac5b858ab7c0be0987db8b50304
Alex Cavender <alexcavender@gmail.com>
2021-07-06 17:23:07 -0400

    deploy

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
           />
           <Project 
             name="Learning React Project #2: Simple Music Player using React Hooks"
-            description="Using React Hooks, I built a simple music player that uses a Playlist + HTMLAUdioElement."
+            description="Using React Hooks, I built a simple music player that uses a JSON Playlist + HTMLAdioElement."
             link="https://448l7.csb.app/"
             imgSrc="/images/reactproject-2.png"
             imgAlt="React Js Project 3"
