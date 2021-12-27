102436368c17b558f69c4f652bdf0be31562e63d
Alex Cavender <alexcavender@gmail.com>
2021-12-27 12:47:48 -0500

    add contact form

--- components/layout.js
+++ components/layout.js
@@ -28,6 +28,41 @@ export default function Layout({ children, home }) {
         `}
       }>
       </script>
+
+      <script src="https://formspree.io/js/formbutton-v1.min.js" defer></script>
+<script>
+  /* paste this line in verbatim */
+  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+  /* customize formbutton below*/     
+  formbutton("create", {
+    action: "https://formspree.io/f/alexcavender@gmail.com",
+    title: "How can we help?",
+    fields: [
+      { 
+        type: "email", 
+        label: "Email:", 
+        name: "email",
+        required: true,
+        placeholder: "your@email.com"
+      },
+      {
+        type: "textarea",
+        label: "Message:",
+        name: "message",
+        placeholder: "What's on your mind?",
+      },
+      { type: "submit" }      
+    ],
+    styles: {
+      title: {
+        backgroundColor: "gray"
+      },
+      button: {
+        backgroundColor: "gray"
+      }
+    }
+  });
+</script>
     </Head>
 
     <header className={styles.header}>
