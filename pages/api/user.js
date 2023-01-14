5fa26752604e1f372107bbdcb339dcbe9c1eea9b
Alex Cavender <alexcavender@gmail.com>
2023-01-13 19:01:30 -0500

    remove old api file

--- pages/api/user.js
+++ pages/api/user.js
@@ -2,6 +2,7 @@ export default function handler(req, res) {
   return res.status(200).json({
     email: "test@person.net",
     name: "Test person",
-    id: 1
+    id: 1,
+    get: "request",
   })
 }
