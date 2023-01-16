8fec70ee029bf906cd5f3e7920abaeede72fe35a
Alex Cavender <alexcavender@gmail.com>
2023-01-15 19:39:56 -0500

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/global.scss'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
